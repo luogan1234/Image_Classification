3369cb74677700618b6b2818036477fb432e3be6
luogan <luogan_62625686@126.com>
2017-05-29 18:48:10 +0800

    testing

---
+++ work/test1.py
@@ -0,0 +1,75 @@
+#coding:UTF-8
+from Data import Data
+from sklearn.ensemble import RandomForestClassifier
+
+def RandomForest(data,feature_train,feature_validation,EST):
+    rfc=RandomForestClassifier(n_estimators=EST)
+    rfc.fit(feature_train,data.label_train)
+    s=rfc.score(feature_validation,data.label_validation)
+    return s
+
+def RandomForestTest(name,EST):
+    data=Data(name)
+    data.readData()
+    train=data.feature_train
+    validation=data.feature_validation
+    test=data.test
+    l=len(train[0])
+    score=[0]*l
+    for i in range(0,l):
+        feature_train=[]
+        feature_validation=[]
+        for a in train:
+            feature_train.append(a[i:(i+1)])
+        for a in validation:
+            feature_validation.append(a[i:(i+1)])
+        score[i]=RandomForest(data,feature_train,feature_validation,EST)
+        if i%256==0:
+            print i
+    ss=[]
+    ss.extend(score)
+    ss.sort()
+    o=0.0
+    for j in range(10,min(512,l)):
+        base=ss[-j]
+        p=[]
+        for i in range(0,l):
+            if score[i]>base:
+                p.append(i)
+        feature_train=[]
+        feature_validation=[]
+        for a in train:
+            d=[]
+            for i in p:
+                d.append(a[i])
+            feature_train.append(d)
+        for a in validation:
+            d=[]
+            for i in p:
+                d.append(a[i])
+            feature_validation.append(d)
+        rfc=RandomForestClassifier(n_estimators=EST)
+        rfc.fit(feature_train,data.label_train)
+        s=rfc.score(feature_validation,data.label_validation)
+        #print base,s
+        if s>o:
+            o=s
+            pp=j
+            res=rfc
+    print o,pp,ss[-pp]
+    feature_test=[]
+    base=ss[-pp]
+    p=[]
+    for i in range(0,l):
+        if score[i]>base:
+            p.append(i)
+    for a in test:
+        d=[]
+        for i in p:
+            d.append(a[i])
+        feature_test.append(d)
+    return res.predict(feature_test)
+
+if __name__ == '__main__':
+    RandomForestTest('bird',33)
+    RandomForestTest('cat',25)
--- work/test2.py
+++ work/test2.py
@@ -1,20 +1,26 @@
 #coding:UTF-8
 from Data import Data
 from sklearn.ensemble import RandomForestClassifier
+import random
+
+EST=49
 
 def RandomForest(name):
     data=Data(name)
     data.readData()
     o=0.0
-    for i in range(5,50):
-        rfc=RandomForestClassifier(n_estimators=i)
+    for i in range(0,1000):
+        rfc=RandomForestClassifier(n_estimators=EST,random_state=random.randint(0,2147483647))
         rfc.fit(data.feature_train,data.label_train)
         s=rfc.score(data.feature_validation,data.label_validation)
         if s>o:
             o=s
             p=i
             res=rfc
-    print o,p
+        print s
+        if i%100==0:
+            print i
+    print o,i
     return res.predict(data.test)
 
 if __name__ == '__main__':
