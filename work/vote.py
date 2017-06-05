7505cf69c021db52d9caab4a91c681480d3d5467
Sleepy_chord <dm_thu@qq.com>
2017-06-05 15:55:43 +0800

    reverse label

--- work/vote.py
+++ work/vote.py
@@ -14,9 +14,9 @@ names1 = ['dm_bird', 'rf100_bird', 'svm_bird', './luogan/bird', 'gdbt_bird']
 ans1 = vote(names1)
 rank = range(len(ans1))
 rank = sorted(rank, key = lambda x: -ans1[x])
-np.save('vote_bird.npy',np.array(ans1 <= ans1[rank[110]]))
+np.save('vote_bird.npy',np.array(ans1 >= ans1[rank[110]]))
 names2 = ['dm_cat', 'rf100_cat', 'svmpoly_cat', './luogan/cat', 'gdbt_cat']
 ans2 = vote(names2)
 rank2 = range(len(ans2))
 rank2 = sorted(rank2, key = lambda x: -ans2[x])
-np.save('vote_cat.npy', np.array(ans2 <= ans2[rank2[130]]))
+np.save('vote_cat.npy', np.array(ans2 >= ans2[rank2[130]]))
